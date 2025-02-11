b990819d7ab8fe8e78a9dc0dfc8e1034ab097eea
dangn511 <dangn511@gmail.com>
2025-02-12 04:12:49 +0800

    Auto-generated code update

--- solution.cpp
+++ solution.cpp
@@ -4,49 +4,72 @@
 
 using namespace std;
 
-class Solution {
-public:
-    void solveSudoku(vector<vector<char>>& board) {
-        solve(board);
+// Function to check if placing 'num' at board[row][col] is valid
+bool isValid(const vector<vector<char>>& board, int row, int col, char num) {
+    // Check the row and column
+    for (int i = 0; i < 9; ++i) {
+        if (board[row][i] == num || board[i][col] == num) {
+            return false;
+        }
     }
 
-private:
-    bool solve(vector<vector<char>>& board) {
-        for (int i = 0; i < 9; ++i) {
-            for (int j = 0; j < 9; ++j) {
-                if (board[i][j] == '.') {
-                    for (char num = '1'; num <= '9'; ++num) {
-                        if (isValid(board, i, j, num)) {
-                            board[i][j] = num; // Place the number
-                            if (solve(board)) {
-                                return true; // Recursively continue to solve
-                            }
-                            board[i][j] = '.'; // Reset and backtrack
+    // Check the 3x3 sub-box
+    int boxRowStart = (row / 3) * 3;
+    int boxColStart = (col / 3) * 3;
+    for (int i = 0; i < 3; ++i) {
+        for (int j = 0; j < 3; ++j) {
+            if (board[boxRowStart + i][boxColStart + j] == num) {
+                return false;
+            }
+        }
+    }
+
+    return true;
+}
+
+// Backtracking function to solve the Sudoku
+bool solveSudoku(vector<vector<char>>& board) {
+    for (int row = 0; row < 9; ++row) {
+        for (int col = 0; col < 9; ++col) {
+            // Find an empty cell
+            if (board[row][col] == '.') {
+                // Try each digit from '1' to '9'
+                for (char num = '1'; num <= '9'; ++num) {
+                    if (isValid(board, row, col, num)) {
+                        // Place the digit
+                        board[row][col] = num;
+
+                        // Recursively attempt to solve the rest of the Sudoku
+                        if (solveSudoku(board)) {
+                            return true;
                         }
+
+                        // Backtrack if placing num doesn't lead to a solution
+                        board[row][col] = '.';
                     }
-                    return false; // No valid number found, need to backtrack
                 }
+                // If no number can be placed, return false
+                return false;
             }
         }
-        return true; // Solved the entire board
     }
 
-    bool isValid(vector<vector<char>>& board, int row, int col, char num) {
-        for (int i = 0; i < 9; ++i) {
-            // Check the row and column
-            if (board[row][i] == num || board[i][col] == num) {
-                return false;
-            }
-            // Check the 3x3 sub-box
-            if (board[3 * (row / 3) + i / 3][3 * (col / 3) + i % 3] == num) {
-                return false;
-            }
+    // If the entire board is filled correctly
+    return true;
+}
+
+// Function to print the Sudoku board
+void printBoard(const vector<vector<char>>& board) {
+    for (const auto& row : board) {
+        for (char cell : row) {
+            cout << cell << " ";
         }
-        return true; // The number is valid
+        cout << endl;
     }
-};
+}
 
 int main() {
+    // Define the Sudoku board
     vector<vector<char>> board = {
         {'5', '3', '.', '.', '7', '.', '.', '.', '.'},
         {'6', '.', '.', '1', '9', '5', '.', '.', '.'},
@@ -59,15 +82,11 @@ int main() {
         {'.', '.', '.', '.', '8', '.', '.', '7', '9'}
     };
 
-    Solution sol;
-    sol.solveSudoku(board);
-
-    // Output the solved board
-    for (const auto& row : board) {
-        for (char c : row) {
-            cout << c << " ";
-        }
-        cout << endl;
+    // Solve the Sudoku puzzle
+    if (solveSudoku(board)) {
+        printBoard(board); // Output the solved Sudoku board
+    } else {
+        cout << "No solution exists!" << endl;
     }
 
     return 0;
