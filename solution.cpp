7d54eb0d8c66917dbea7e85b98bbca5281b36fd5
dangn511 <dangn511@gmail.com>
2025-02-12 04:31:04 +0800

    Auto-generated code update

--- solution.cpp
+++ solution.cpp
@@ -4,84 +4,84 @@
 
 using namespace std;
 
-class SudokuSolver {
+class Solution {
 public:
     void solveSudoku(vector<vector<char>>& board) {
         solve(board);
     }
-
+    
 private:
+    // Function to solve the sudoku using backtracking
     bool solve(vector<vector<char>>& board) {
-        for (int row = 0; row < 9; row++) {
-            for (int col = 0; col < 9; col++) {
-                // Check for an empty cell
+        for (int row = 0; row < 9; ++row) {
+            for (int col = 0; col < 9; ++col) {
+                // If we find an empty cell, try to fill it
                 if (board[row][col] == '.') {
-                    // Try all possible digits from '1' to '9'
-                    for (char num = '1'; num <= '9'; num++) {
+                    for (char num = '1'; num <= '9'; ++num) {
+                        // Check if placing num is valid
                         if (isValid(board, row, col, num)) {
-                            // Place the number
-                            board[row][col] = num;
+                            board[row][col] = num; // Place num
 
-                            // Continue to solve the rest of the board
+                            // Recursively try to fill in the rest
                             if (solve(board)) {
-                                return true;
+                                return true; // If successful, return true
                             }
-
-                            // Backtrack if unsuccessful
+                            
+                            // If placing num didn't lead to a solution, reset the cell
                             board[row][col] = '.';
                         }
                     }
-                    return false; // Trigger backtracking
+                    return false; // No valid number found, return false
                 }
             }
         }
-        return true; // Puzzle solved
+        return true; // Sudoku solved
     }
-
-    bool isValid(const vector<vector<char>>& board, int row, int col, char num) {
-        // Check the row
-        for (int i = 0; i < 9; i++) {
-            if (board[row][i] == num) return false;
-        }
-
-        // Check the column
-        for (int i = 0; i < 9; i++) {
-            if (board[i][col] == num) return false;
-        }
-
-        // Check the 3x3 sub-box
-        int startRow = row - row % 3;
-        int startCol = col - col % 3;
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                if (board[i + startRow][j + startCol] == num) return false;
+    
+    // Function to check if placing num at board[row][col] is valid
+    bool isValid(vector<vector<char>>& board, int row, int col, char num) {
+        // Check the row and column
+        for (int i = 0; i < 9; ++i) {
+            if (board[row][i] == num || board[i][col] == num) {
+                return false;
             }
         }
-
-        return true; // Valid placement
+        
+        // Check the 3x3 sub-box
+        int startRow = row / 3 * 3;
+        int startCol = col / 3 * 3;
+        for (int i = startRow; i < startRow + 3; ++i) {
+            for (int j = startCol; j < startCol + 3; ++j) {
+                if (board[i][j] == num) {
+                    return false;
+                }
+            }
+        }
+        
+        return true; // Placing num is valid
     }
 };
 
 int main() {
+    Solution solution;
     vector<vector<char>> board = {
-        {'5','3','.','.','7','.','.','.','.'},
-        {'6','.','.','1','9','5','.','.','.'},
-        {'.','9','8','.','.','.','.','6','.'},
-        {'8','.','.','.','6','.','.','.','3'},
-        {'4','.','.','8','.','3','.','.','1'},
-        {'7','.','.','.','2','.','.','.','6'},
-        {'.','6','.','.','.','.','2','8','.'},
-        {'.','.','.','4','1','9','.','.','5'},
-        {'.','.','.','.','8','.','.','7','9'}
+        {'5', '3', '.', '.', '7', '.', '.', '.', '.'},
+        {'6', '.', '.', '1', '9', '5', '.', '.', '.'},
+        {'.', '9', '8', '.', '.', '.', '.', '6', '.'},
+        {'8', '.', '.', '.', '6', '.', '.', '.', '3'},
+        {'4', '.', '.', '8', '.', '3', '.', '.', '1'},
+        {'7', '.', '.', '.', '2', '.', '.', '.', '6'},
+        {'.', '6', '.', '.', '.', '.', '2', '8', '.'},
+        {'.', '.', '.', '4', '1', '9', '.', '.', '5'},
+        {'.', '.', '.', '.', '8', '.', '.', '7', '9'}
     };
-
-    SudokuSolver solver;
-    solver.solveSudoku(board);
-
-    // Output the solved Sudoku board
+    
+    solution.solveSudoku(board);
+    
+    // Print the solved Sudoku board
     for (const auto& row : board) {
-        for (char cell : row) {
-            cout << cell << " ";
+        for (const auto& num : row) {
+            cout << num << ' ';
         }
         cout << endl;
     }
